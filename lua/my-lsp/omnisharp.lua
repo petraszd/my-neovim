f146059420f10df3223f2a3188619512622b96e4
Petras Zdanavičius <petraszd@gmail.com>
2022-01-27 00:02:36 +0200

    omnisharp

--- lua/my-lsp/omnisharp.lua
+++ lua/my-lsp/omnisharp.lua
@@ -1,5 +1,9 @@
 local lsp_cfg = require('lspconfig')
+local lsp_util = require('lspconfig.util')
 
 lsp_cfg.omnisharp.setup({
-  cmd = { "/usr/local/bin/omnisharp", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) };
+  cmd = { "/usr/local/bin/omnisharp", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) },
+  root_dir = function(fname)
+    return lsp_util.root_pattern("*.csproj", "*.sln")(fname)
+  end,
 })
