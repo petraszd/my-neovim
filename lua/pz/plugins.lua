b34218e6a47433bb4cfcc9ea679c8ca2a3255c13
Petras <petras@onecodex.com>
2023-05-22 23:25:36 +0300

    Lualine

--- lua/pz/plugins.lua
+++ lua/pz/plugins.lua
@@ -55,6 +55,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { { "filename", path = 1 } },
+        lualine_c = {},
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+      },
     },
   },
 
