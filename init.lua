501e505cd7773e1bccf66d45d6c3bec41d6942c8
Petras <petras@onecodex.com>
2024-03-22 10:08:41 +0200

    Hl on yanking

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
+vim.opt.showmode = false
 
 vim.keymap.set("n", "<F2>", function ()
   package.loaded["pz/colors"] = nil
@@ -43,6 +44,16 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.opt.tabstop = 2
     vim.opt.shiftwidth = 2
+  end,
+})
+
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
   end,
 })
 
